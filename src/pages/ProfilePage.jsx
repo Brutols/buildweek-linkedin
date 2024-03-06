252ceea6d87fb668303f65cd3a28f5872d12af16
Brutols <dtwo97@gmail.com>
2024-03-06 20:46:23 +0100

    pre merge

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -3,29 +3,9 @@ import ButtonLogout from '../components/buttonLogout/ButtonLogout';
 import { useParams } from "react-router-dom";
 const ProfilePage = () => {
 
-  const key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWU3OWZiMDhlMWMzYTAwMTkyMjg3YTQiLCJpYXQiOjE3MDk2Nzg1MTMsImV4cCI6MTcxMDg4ODExM30.A-OQ2-69n0g_XZ79JWwnk-G8OooyZ9k8fe7no4E7k4g'
-  const [personalProfile, setPersonalProfile] = useState({});
+  // const key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWU3OWZiMDhlMWMzYTAwMTkyMjg3YTQiLCJpYXQiOjE3MDk2Nzg1MTMsImV4cCI6MTcxMDg4ODExM30.A-OQ2-69n0g_XZ79JWwnk-G8OooyZ9k8fe7no4E7k4g'
+
   const {id} = useParams()
-
-  console.log(personalProfile);
-
-  useEffect(() => {
-    const getProfile = async () => {
-      const result = await fetch(
-        `https://striveschool-api.herokuapp.com/api/profile/${id}`,
-        {
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${key}`,
-          },
-        }
-      );
-      const data = await result.json();
-      setPersonalProfile(data);
-      
-    };
-    getProfile();
-  }, [id]);
 
   return (
     <>
