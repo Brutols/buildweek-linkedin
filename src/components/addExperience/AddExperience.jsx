dbefa1785e065859bb91d245abd484b4c0056884
Brutols <dtwo97@gmail.com>
2024-03-07 21:38:11 +0100

    experience post + education

--- src/components/addExperience/AddExperience.jsx
+++ src/components/addExperience/AddExperience.jsx
@@ -4,15 +4,38 @@ import styles from "./addExperience.module.css";
 import CustomButton from "../button/Button";
 import { Form } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
-import { isShow, setShow } from "../../Slices/experiencesSlice";
+import {
+  allFormData,
+  isShow,
+  postExperience,
+  resetFormData,
+  setFormData,
+  setShow,
+} from "../../Slices/experiencesSlice";
 import ButtonIcon from "../buttonIcon/ButtonIcon";
 
-function AddExperience() {
+function AddExperience({id}) {
   const show = useSelector(isShow);
+  const formData = useSelector(allFormData);
   const dispatch = useDispatch();
+
+  const experiencesUrl = `https://striveschool-api.herokuapp.com/api/profile/${id}/experiences`;
 
   const handleClose = () => dispatch(setShow());
   const handleShow = () => dispatch(setShow());
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    console.log(name, value);
+    dispatch(setFormData({ name: name, value: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(formData);
+    dispatch(postExperience({url: experiencesUrl, data: formData}))
+    dispatch(resetFormData())
+  }
 
   return (
     <>
@@ -25,27 +48,51 @@ function AddExperience() {
           <Offcanvas.Title>Add Experience</Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body>
-          <Form>
+          <Form className="d-flex flex-column gap-2" onSubmit={handleSubmit}>
             <FormControl
               className={styles.myInput}
               type="text"
               placeholder="role"
+              name="role"
+              value={formData.role}
+              onChange={handleChange}
             />
             <FormControl
               className={styles.myInput}
               type="text"
               placeholder="company"
+              name="company"
+              value={formData.company}
+              onChange={handleChange}
             />
-            <FormControl className={styles.myInput} type="date" />
-            <FormControl className={styles.myInput} type="date" />
+            <FormControl
+              className={styles.myInput}
+              type="date"
+              name="startDate"
+              value={formData.startDate}
+              onChange={handleChange}
+            />
+            <FormControl
+              className={styles.myInput}
+              type="date"
+              name="endDate"
+              value={formData.endDate}
+              onChange={handleChange}
+            />
             <FormControl
               className={styles.myInput}
               type="text"
               placeholder="description"
+              name="description"
+              value={formData.description}
+              onChange={handleChange}
             />
             <Form.Select
               aria-label="Default select example"
+              name="area"
+              value={formData.area}
               className={styles.myInput}
+              onChange={handleChange}
             >
               <option>Work / Education</option>
               <option value="work">Work</option>
--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -4,24 +4,18 @@ import MainLayout from "../layouts/MainLayout";
 import { useDispatch, useSelector } from "react-redux";
 import { allProfiles, getProfiles } from "../Slices/profilesSlice";
 import { useParams } from "react-router-dom";
-import { allExperiences, getExperiences } from "../Slices/experiencesSlice";
 import { Container } from "react-bootstrap";
 import ExperiencesCard from "../components/experiencesCard/ExperiencesCard";
 
 const ProfilePage = () => {
   const dispatch = useDispatch();
   const profiles = useSelector(allProfiles);
-  const experiences = useSelector(allExperiences);
   const { id } = useParams();
-  console.log(experiences);
 
   const profilesUrl = `https://striveschool-api.herokuapp.com/api/profile/${id}`;
-  const experiencesUrl = `https://striveschool-api.herokuapp.com/api/profile/${id}/experiences`;
-
   useEffect(() => {
     dispatch(getProfiles(profilesUrl));
-    dispatch(getExperiences(experiencesUrl));
-  }, [dispatch, profilesUrl, experiencesUrl]);
+  }, [dispatch, profilesUrl]);
 
   return (
     <>
@@ -30,7 +24,8 @@ const ProfilePage = () => {
         <ButtonLogout /> */}
         <Container className="row mx-auto">
           <div className="col-9 p-5">
-          <ExperiencesCard />
+          <ExperiencesCard id={id} text="Experience" variant="experience" />
+          <ExperiencesCard id={id} text="Education" variant="education" />
           </div>
           <div className="col-3"></div>
         </Container>    
